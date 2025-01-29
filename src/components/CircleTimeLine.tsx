8855ec1a657f563fcb13bbd42b40e7636f66100f
Ekaterina <kate8190758@gmail.com>
2025-01-30 02:10:44 +0400

    feat: The layout and styles of the main circle have been add

---
+++ src/components/CircleTimeLine.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+interface TimelinePoint {
+  id: number;
+  angle: number;
+}
+
+const POINTS: TimelinePoint[] = [
+  { id: 1, angle: 60 },
+  { id: 2, angle: 120 },
+  { id: 3, angle: 180 },
+  { id: 4, angle: 240 },
+  { id: 5, angle: 300 },
+  { id: 6, angle: 360 },
+];
+
+export const CircleTimeline: React.FC = () => {
+  const calculatePosition = (angle: number) => {
+    const radius = 265;
+    const centerX = radius;
+    const centerY = radius;
+
+    const radian = (angle - 90) * (Math.PI / 180);
+    const x = centerX + radius * Math.cos(radian);
+    const y = centerY + radius * Math.sin(radian);
+
+    return { x, y };
+  };
+
+  return (
+    <div className="circle-timeline">
+      <div className="circle-container">
+        <div className="main-circle"></div>
+
+        {POINTS.map((point) => {
+          const position = calculatePosition(point.angle);
+          return (
+            <div
+              key={point.id}
+              className={`segment-point point-${point.id}`}
+              style={{
+                left: `${position.x}px`,
+                top: `${position.y}px`,
+              }}
+            ></div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
--- src/components/TimeLine.tsx
+++ src/components/TimeLine.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { TimelineWrapper } from "./TimelineWrapper";
-
+import { CircleTimeline } from "./CircleTimeLine";
 
 export const Timeline: React.FC = () => {
   return (
     <TimelineWrapper>
-      <div className="timeline-content"></div>
+      <CircleTimeline />
     </TimelineWrapper>
   );
 };
-
-
